0896764223487eddc7501059b61f96f4b93d487a
Filan <fedosenko009@mail.ru>
2025-06-10 21:10:31 +0300

    Base Template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <!--  Шаблон страницы  -->
+    <header class="header_relative">
+        <RouterLink to="/">Home</RouterLink>
+    </header>
+    <RouterView />
+</template>
+
+<script setup lang="ts">
+    import { RouterLink, RouterView } from 'vue-router';
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <!--  Главная страница  -->
+    <main>
+        хуй
+    </main>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
