638faf57aa33f7c1d374079a74bb13fc8ee8bf89
Centick <utyatsky.ry@yandex.ru>
2025-06-11 15:35:05 +0300

    service + project 20%

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,6 +29,105 @@
                 </div>
             </div>
         </section>
+
+        <section class="section">
+            <div class="container grid justyfy">
+                <h2 class="h2">Услуги</h2>
+                <div class="wrap_services grid gap-25">
+                    <div class="block_service block_service--1">
+                        <h3 class="h3">Landin page</h3>
+                        <span></span>
+                    </div>
+                    <div class="block_service block_service--2">
+                        <h3 class="h3">E-commerce</h3>
+                        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, quos sint? Laudantium, ducimus beatae! Quae repellendus eum sequi eius harum, facere rem recusandae illo quisquam, corrupti, voluptatum cupiditate tempora excepturi.</span>
+                    </div>
+                    <div class="block_service block_service--3">
+                        <h3 class="h3">Интернет магазин</h3>
+                        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, quos sint? Laudantium, ducimus beatae! Quae repellendus eum sequi eius harum, facere rem recusandae illo quisquam, corrupti, voluptatum cupiditate tempora excepturi.</span>
+                    </div>
+                    <div class="block_service block_service--4">
+                        <h3 class="h3">Интернет магазин</h3>
+                        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, quos sint? Laudantium, ducimus beatae! Quae repellendus eum sequi eius harum, facere rem recusandae illo quisquam, corrupti, voluptatum cupiditate tempora excepturi.</span>
+                    </div>
+                    <div class="block_service block_service--5">
+                        <h3 class="h3">Интернет магазин</h3>
+                        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, quos sint? Laudantium, ducimus beatae! Quae repellendus eum sequi eius harum, facere rem recusandae illo quisquam, corrupti, voluptatum cupiditate tempora excepturi.</span>
+                    </div>
+                    <div class="block_service block_service--6">
+                        <h3 class="h3">lorem lorem</h3>
+                        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, quos sint? Laudantium, ducimus beatae! Quae repellendus eum sequi eius harum, facere rem recusandae illo quisquam, corrupti, voluptatum cupiditate tempora excepturi.</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <h2 class="h2">Кейсы</h2>
+                <div class="wrap_projects grid gap-25 width-100">
+                    <div class="block_project grid grid-column gap-15">
+                        <div class="view_content width-100">
+                            <img class="width-100" src="https://i.pinimg.com/originals/19/89/44/198944ea9f57d70ea850fc868efbd4b6.jpg" alt="img">
+                            <!-- video -->
+                        </div>
+                        <h4 class="h4">Advent-календарь от КИБЕРПРОТЕКТ</h4>
+                        <p class="h6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente mollitia quidem odit minima dolorum reprehend</p>
+                        <div class="wrap_character_project grid grid-row gap-10">
+                            <span class="block_character_project">lorem</span>
+                            <span class="block_character_project">lorem lor</span>
+                            <span class="block_character_project">loremrere</span>
+                        </div>
+                    </div>
+                    <div class="block_project grid grid-column gap-15">
+                        <div class="view_content width-100">
+                            <img class="width-100" src="https://i.pinimg.com/originals/19/89/44/198944ea9f57d70ea850fc868efbd4b6.jpg" alt="img">
+                            <!-- video -->
+                        </div>
+                        <h4 class="h4">Advent-календарь от КИБЕРПРОТЕКТ</h4>
+                        <p class="h6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente mollitia quidem odit minima dolorum reprehend</p>
+                        <div class="wrap_character_project grid grid-row gap-10">
+                            <span class="block_character_project">lorem</span>
+                            <span class="block_character_project">lorem lor</span>
+                            <span class="block_character_project">loremrere</span>
+                        </div>
+                    </div>
+                    <div class="block_project grid grid-column gap-15">
+                        <div class="view_content width-100">
+                            <img class="width-100" src="https://i.pinimg.com/originals/19/89/44/198944ea9f57d70ea850fc868efbd4b6.jpg" alt="img">
+                            <!-- video -->
+                        </div>
+                        <h4 class="h4">Advent-календарь от КИБЕРПРОТЕКТ</h4>
+                        <p class="h6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente mollitia quidem odit minima dolorum reprehend</p>
+                        <div class="wrap_character_project grid grid-row gap-10">
+                            <span class="block_character_project">lorem</span>
+                            <span class="block_character_project">lorem lor</span>
+                            <span class="block_character_project">loremrere</span>
+                        </div>
+                    </div>
+                    <div class="block_project grid grid-column gap-15">
+                        <div class="view_content width-100">
+                            <img class="width-100" src="https://i.pinimg.com/originals/19/89/44/198944ea9f57d70ea850fc868efbd4b6.jpg" alt="img">
+                            <!-- video -->
+                        </div>
+                        <h4 class="h4">Advent-календарь от КИБЕРПРОТЕКТ</h4>
+                        <p class="h6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente mollitia quidem odit minima dolorum reprehend</p>
+                        <div class="wrap_character_project grid grid-row gap-10">
+                            <span class="block_character_project">lorem</span>
+                            <span class="block_character_project">lorem lor</span>
+                            <span class="block_character_project">loremrere</span>
+                        </div>
+                    </div>
+                </div>
+                <button class="btn">Все проекты</button>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <h3></h3>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -41,6 +140,11 @@
         border: 1px var(--colorMain);
         color: var(--colorWhite);
     } */
+
+    header{
+        background-color: var(--colorWhite);
+        z-index: 5;
+    }
 
     .section-banner{
         width: 100vw;
@@ -55,5 +159,70 @@
         padding: 10px 18px;
         border: 1px rgba(128, 128, 128, 0.5) solid;
         border-radius: var(--borderRadius);
+    }
+
+    .wrap_services{
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(5, 1fr);
+        grid-template-areas:
+            "one two four "
+            "one two four"
+            "one three four"
+            "five three four"
+            "five three button";
+        max-height: 700px;
+    }
+
+    .block_service{
+        padding: 30px;
+        border-radius: var(--borderRadius);
+        background-color: #d9d9d9;
+    }   
+
+    .block_service--1{
+        grid-area: one;
+    }
+
+    .block_service--2{
+        grid-area: two;
+    }
+
+    .block_service--3{
+        grid-area: three;
+    }
+    .block_service--4{
+        grid-area: four;
+    }
+    .block_service--5{
+        grid-area: five;
+    }
+    .block_service--6{
+        grid-area: button;
+    }
+
+    /* projects */
+    .wrap_projects{
+        grid-template-columns: 1fr 1fr;
+    }
+
+    .view_content{
+        /* height: 500px; */
+        border-radius: var(--borderRadius);
+        overflow: hidden;
+    }
+
+    .view_content > img{
+        object-fit: cover;
+    }
+
+    .block_character_project{
+        padding: 10px 18px;
+        border: 1px rgba(128, 128, 128, 0.5) solid;
+        border-radius: var(--borderRadius);
+        width: fit-content;
+    }
+
+    .wrap_character_project{
+        width: fit-content;
     }
 </style>
